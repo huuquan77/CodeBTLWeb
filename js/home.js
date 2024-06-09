1d27a0eaf34d03a4e8ece7879f832ddc9649cd7c
Tobii1707 <hanamkhanh2004@gmail.com>
2024-06-09 23:06:06 +0700

    update

--- js/home.js
+++ js/home.js
@@ -1,25 +1,24 @@
 const leftArrow = document.getElementById('left');
-        const rightArrow = document.getElementById('right');
-        const carousel = document.querySelector('.carousel');
+const rightArrow = document.getElementById('right');
+const carousel = document.querySelector('.carousel');
 
-        rightArrow.addEventListener('click', () => {
-            carousel.scrollBy({ left: carousel.offsetWidth, behavior: 'smooth' });
-        });
+rightArrow.addEventListener('click', () => {
+    carousel.scrollBy({ left: carousel.offsetWidth, behavior: 'smooth' });
+});
 
-        leftArrow.addEventListener('click', () => {
-            carousel.scrollBy({ left: -carousel.offsetWidth, behavior: 'smooth' });
-        });
+leftArrow.addEventListener('click', () => {
+    carousel.scrollBy({ left: -carousel.offsetWidth, behavior: 'smooth' });
+});
 
-        function autoScroll() {
-            carousel.scrollBy({ left: carousel.offsetWidth, behavior: 'smooth' });
+function autoScroll() {
+    if (carousel.scrollLeft + carousel.offsetWidth >= carousel.scrollWidth) {
+        // If at the end of the carousel, scroll back to start immediately
+        carousel.scrollTo({ left: 0, behavior: 'smooth' });
+    } else {
+        // Otherwise, scroll to the next image
+        carousel.scrollBy({ left: carousel.offsetWidth, behavior: 'smooth' });
+    }
+}
 
-            // If at the end of the carousel, scroll back to start
-            if (carousel.scrollLeft + carousel.offsetWidth >= carousel.scrollWidth) {
-                setTimeout(() => {
-                    carousel.scrollTo({ left: 0, behavior: 'smooth' });
-                }, 3000); // 3 seconds delay before resetting to the start
-            }
-        }
-
-        // Auto scroll every 3 seconds
-        setInterval(autoScroll, 3000);
+// Auto scroll every 3 seconds
+setInterval(autoScroll, 3000);
